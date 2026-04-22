046f3ee114ab1186ca94eb1b7fdaf88ce6848a25
ZhangKunze <ZhangKunze@users.noreply.github.com>
2026-04-22 19:35:11 +0800

    Initial commit: nvidia.c 包管理器风格骂 NVIDIA

---
+++ nvidia.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(){
+printf("正在读取软件包列表... 完成\n");
+printf("正在分析软件包依赖关系...\n");
+printf("正在读取状态信息... 完成\n");
+sleep(2);
+printf("\033[1;31m错误: \033[0mdpkg: NVIDIA, fuck you!\n");
+sleep(2);
+printf("\033[1;33m警告: \033[0mapt: 因为 NVIDIA 不开源 Linux 驱动\n");
+sleep(2);
+printf("\033[1;33m警告: \033[0mdpkg: 开头为[*]的行会输出较多内容 \n");
+sleep(2);
+printf("\033[1;33m[*]: \033[0mdpkg: NVIDIA 对 Linux 社区的态度很敷衍。2012年,有一个学者向 Linus Torvalds 提出了这个问题, Linus 忍无可忍无需再忍,诞生了开源圣经“so NVIDIA,fuck you!”\n");
+sleep(4);
+printf("\033[1;31m错误: \033[0mdpkg: 无法安装 “nvidia-drive-*” 包\n");
+}
